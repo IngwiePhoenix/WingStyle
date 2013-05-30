3ea11db900285dc895ae4d38db8ce4f4e14559fa
Glen Murray <gmurray@viatropolis.com>
2013-05-29 23:28:34 -0400

    Border Radius and Text Shadow
    New WingStyles to look at.

---
+++ rulesets/WS_textshadow.php
@@ -0,0 +1,18 @@
+<?php class WS_textshadow extends WingStyleDesigner {
+
+ public function linear($hshadow, $vshadow, $blur, $color) {
+  $this->addRule(new WingStyleRule("text-shadow",implode(" ",func_get_args())));
+  return $this->WS(); #Return the main instance - very important!!!
+ }
+
+} ?>
+
+
+
+
+
+
+
+
+
+
